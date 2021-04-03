3ada78b2abd154daee56b27930dc9123e86599b6
lijar-B <64573338+lijar-B@users.noreply.github.com>
2021-04-03 21:58:10 +0630

    first commit

---
+++ tuto.js
@@ -0,0 +1,22 @@
+const words = ['Hello Guys', 'I am Li Jar', 'Engineer', 'Web developer', 'My quote is..Passion Made Possible!'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+    if (count === words.length){
+        count=0;
+    }
+    
+    currentText = words[count];
+    letter = currentText.slice(0, ++index);
+    
+    document.querySelector(".typing").textContent=letter;
+    if (letter.length===currentText.length){
+        count++;
+        index=0;
+    }
+    setTimeout(type,400);
+})();
